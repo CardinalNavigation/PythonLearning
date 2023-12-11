599c67979297b606e79cdee4cc6f78d1a89ef9d7
Matthew Johnson <mtthw.jhnson@gmail.com>
2023-12-11 10:37:47 -0600

    Woop, now we are cooking, Python3 is the command on MacOS

---
+++ text.py
@@ -0,0 +1,13 @@
+## This is a Test Comment
+"""
+This is also a test Commment
+That can go down multiple lines
+"""
+
+hello_world="hello world"
+print(hello_world)
+b= 1, 2, 3, 4
+print(b)
+c= ["Hello", "World"]
+print(c)
+print(c[0])
